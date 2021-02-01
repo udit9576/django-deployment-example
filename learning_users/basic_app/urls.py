f4de8641c4f52505a91d0f2b37fce6ea12d925e5
udit <udit_kinger2156@yahoo.com>
2021-02-01 09:52:21 +0530

    first

---
+++ learning_users/basic_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from basic_app import views
+
+app_name = 'basic_app'
+
+urlpatterns = [
+    path('user_login/', views.user_login, name='user_login'),
+    path('register/', views.register, name='register'),
+]
